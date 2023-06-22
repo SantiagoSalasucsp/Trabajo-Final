1bb28ad7a40ed53aa8463156bb434dfc98c3041f
Santiago <santiago.salas.sotillo@ucsp.edu.pe>
2023-06-22 00:49:44 -0500

    trabajo fianl, mas final que nunca

---
+++ quinto/Partitura.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include "Partitura.h"
+
+using namespace std;
+
+Partitura::Partitura(){
+
+}
+
+void Partitura::tocar(){
+
+
+}
--- quinto/Partitura.h
+++ quinto/Partitura.h
@@ -7,11 +7,31 @@
 
 class Partitura{
 
+
+    public:
     Partitura();
 
     void tocar();
 
+    private:
 
+    int Do=0;
+    int Reb=1;
+    int Re=2;
+    int Mib=3;
+    int Mi=4;
+    int Fa=5;
+    int Solb=6;
+    int Sol=7;
+    int Lab=8;
+    int La=9;
+    int Sib=10;
+    int Si=11;
+
+
+    int blanca=1400;
+    int negra =700;
+    int corchea =350;
 
 
 
